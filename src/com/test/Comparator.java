555df6720b45118dae827827e3946f497cd339ee
gerriegash <gerrie.gash@gmail.com>
2017-09-04 14:48:26 +0530

    Making comparator chained API.

--- src/com/test/Comparator.java
+++ src/com/test/Comparator.java
@@ -14,8 +14,25 @@ public interface Comparator<T> {
 	
 	public int compare(T t1, T t2);
 	
-	public static Comparator<Person> comparing(Function<Person,Comparable> comparingFunction) {
-		return (p1,p2) -> comparingFunction.apply(p1).compareTo(comparingFunction.apply(p2));
+	public static <T> Comparator<T> comparing(Function<T, Comparable> comparingFunction) {
+		return (p1, p2) -> comparingFunction.apply(p1).compareTo(comparingFunction.apply(p2));
 	}
+
+	public default Comparator<T> thenComparing(Function<T,Comparable> comparingFunction) {
+//		return (p1,p2) -> 
+//		{
+//			if(compare(p1,p2) == 0) {
+//				return compare(p1,p2);
+//			} else {
+//				return comparingFunction.apply(p1).compareTo(comparingFunction.apply(p2));
+//			}
+//	};
+	return thenComparing(comparing(comparingFunction));
+	}
+	
+	public default Comparator<T> thenComparing(Comparator<T> comp) {
+			return (p1,p2) -> compare(p1, p2) ==0?comp.compare(p1, p2):compare(p1, p2);
+	}
+	
 	
 }
